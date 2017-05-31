4b763296604b8cd24a6e4ed4e6af4232e8ad1d82
David Ramsington <grokbot.dwr@gmail.com>
2017-05-31 11:05:56 -0400

    Add some app stuff

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -10,7 +10,19 @@
                     <div class="panel-heading">Dashboard</div>
 
                     <div class="panel-body">
-                        Your application's dashboard.
+                        <h2>You have no blackmail currently.</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Create new BlackMail</div>
+
+                    <div class="panel-body">
+                        <input type="file" class="form-control" /><br/>
+                        <a href="/blackmail" class="btn btn-primary">Submit</a>
                     </div>
                 </div>
             </div>
